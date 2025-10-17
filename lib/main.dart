ba75c30711efbf2ec96b8f3bb95d97cd2e8a94b2
Niks Martinsons <66245053+nmartinsons@users.noreply.github.com>
2025-10-17 18:36:00 +0300

    Commented out unused

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
-import 'package:firebase_ai/firebase_ai.dart';
+// import 'package:firebase_ai/firebase_ai.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
