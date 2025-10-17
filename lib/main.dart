fe207310db582965d25488b6d71f8c2ad668ab07
Niks Martinsons <66245053+nmartinsons@users.noreply.github.com>
2025-10-17 16:43:53 +0300

    Added Firebase to the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   await Supabase.initialize(
     url: 'https://zsqswroaevuxqdamdykl.supabase.co',
     anonKey:
